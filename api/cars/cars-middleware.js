b9448c0728fdd8ce5301ba0cb497e2210e728b80
MarkLWorkman <70355546+MarkLWorkman@users.noreply.github.com>
2021-05-23 11:35:38 -0400

    checkCarId middleware complete

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,29 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const cars = require("./cars-model");
+const vinValidator = require("vin-validator");
+const Error = require("../Error");
+
+const checkCarId = async (req, res, next) => {
+  try {
+    const car = await cars.getById(req.params.id);
+    if (car) {
+      req.car = car;
+      next();
+    } else {
+      next(new Error(`Car with id ${req.params.id} was not found`, 404));
+    }
+  } catch (error) {
+    next(new Error(error, 500));
+  }
+};
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
